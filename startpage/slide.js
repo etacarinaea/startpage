2eb94c0df2440612b6a27bf677ef8059c7150ef6
yuki <yuki77x@googlemail.com>
2014-04-24 00:45:09 +0200

    startpage

---
+++ startpage/slide.js
@@ -0,0 +1,10 @@
+function expand(x){
+	x.style.height=450+"px";
+	x.style.borderTop="2px solid #e64747";
+	x.style.borderBottom="2px solid #e64747";
+}
+function contract(x){
+	x.style.height="150px";
+	x.style.borderTop="0 solid #e64747";
+	x.style.borderBottom="0 solid #e64747";
+}
