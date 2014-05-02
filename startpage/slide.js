38128df2183b94ee7049c5ab897ab242499a08f7
yuki <yuki77x@googlemail.com>
2014-05-03 00:42:53 +0200

    font and colour change
    changed colours
    changed span font to cure.se

--- startpage/slide.js
+++ startpage/slide.js
@@ -1,11 +1,11 @@
 function expand(x){
 	var acount = x.getElementsByTagName('a').length;
 	x.style.height=300+25*acount+"px";
-	x.style.borderTop="2px solid #e64747";
-	x.style.borderBottom="2px solid #e64747";
+	x.style.borderTop="2px solid #ad3636";
+	x.style.borderBottom="2px solid #ad3636";
 }
 function contract(x){
 	x.style.height="150px";
-	x.style.borderTop="0 solid #e64747";
-	x.style.borderBottom="0 solid #e64747";
+	x.style.borderTop="0 solid #ad3636";
+	x.style.borderBottom="0 solid #ad3636";
 }
