d089e8b99dbc82015c95dbc6522d1a1629c24c33
yuki <yuki77x@googlemail.com>
2014-05-03 03:36:57 +0200

    removed inline events
    chrome

--- startpage/slide.js
+++ startpage/slide.js
@@ -1,11 +1,19 @@
-function expand(x){
-	var acount = x.getElementsByTagName('a').length;
-	x.style.height=300+25*acount+"px";
-	x.style.borderTop="2px solid #ad3636";
-	x.style.borderBottom="2px solid #ad3636";
-}
-function contract(x){
-	x.style.height="150px";
-	x.style.borderTop="0 solid #ad3636";
-	x.style.borderBottom="0 solid #ad3636";
-}
+document.addEventListener("DOMContentLoaded", function() {
+    var sqr = document.querySelectorAll(".sqr");
+	for (var i = 0; i < sqr.length; ++i) {
+		sqr[i].addEventListener("mouseover", expand, false);
+		sqr[i].addEventListener("mouseout", contract, false);
+	}
+}, false);
+
+function expand(){
+	var acount = this.getElementsByTagName('a').length;
+	this.style.height=300+25*acount+"px";
+	this.style.borderTop="2px solid #ad3636";
+	this.style.borderBottom="2px solid #ad3636";
+};
+function contract(){
+	this.style.height="150px";
+	this.style.borderTop="0 solid #ad3636";
+	this.style.borderBottom="0 solid #ad3636";
+};
