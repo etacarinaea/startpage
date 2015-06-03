ddee429620e9ac0e1c7879e1027e6554e8cb4d34
yuki <yuki77x@googlemail.com>
2015-06-04 01:34:04 +0200

    add popup that shows a list of search options
    Shows a popup when entering -h into the search field.
    Can be removed by entering -h again or clicking on it.
    Also added some comments.

--- main.js
+++ main.js
@@ -21,23 +21,27 @@ $.getJSON("config.json", function(data){
     ];
     window.cfg_bool = [
         data.bool.borders,
-        data.bool.simplesearch,
         data.bool.alwaysopen,
         data.bool.mascot
     ];
     $("span").css("fontFamily", cfg[0]);
     $("a").css("fontFamily", cfg[1]);
+    $("#popup").css("fontFamily", cfg[1]);
     $("span").css("fontSize", cfg[2]);
     $("a").css("fontSize", cfg[3]);
+    $("#popup").css("fontSize", cfg[3]);
     $("body").css("backgroundColor", cfg[4]);
     $(".sqr").css("backgroundColor", cfg[5]);
+    $("#popup").css("backgroundColor", cfg[5]);
     $("span").css("color", cfg[6]);
     $("a").css("color", cfg[7]);
+    $("#popup").css("color", cfg[7]);
     $(".sqr").css("borderTop", "0 solid " + cfg[8]);
     $(".sqr").css("borderBottom", "0 solid " + cfg[8]);
+    $("#popup").css("borderTop", cfg[9] + " solid " + cfg[8]);
     $("#searchinput").css("color", cfg[10]);
     $("#searchinput").css("backgroundColor", cfg[11]);
-    if(cfg_bool[3]){
+    if(cfg_bool[2]){
         $("#bgimg").css("backgroundImage", "url('" +  cfg[12] + "')");
         $("#bgimg").css("bottom", cfg[13]);
         $("#bgimg").css("right", cfg[14]);
@@ -51,10 +55,22 @@ $.getJSON("config.json", function(data){
 
 function evenContainerHeight(){
 	if(window.innerHeight % 2 ==0){
-		document.getElementById("container").style.height = window.innerHeight;
+		document.getElementById("container").style.height =
+            window.innerHeight;
 	}else{
-		document.getElementById("container").style.height = window.innerHeight - 1;
+		document.getElementById("container").style.height =
+            window.innerHeight - 1;
 	}
+}
+
+
+function popup(obj, msg, visibility){
+    if(!visibility){
+        obj.innerHTML = msg;
+        obj.style.bottom = "-" + cfg[9];
+    }else{
+        obj.style.bottom = -200 + "px";
+    }
 }
 
 window.onresize = function(){
@@ -63,44 +79,62 @@ window.onresize = function(){
 
 window.onload = function(){
 	evenContainerHeight();
-    document.getElementById("searchinput").addEventListener("keypress", function search(a){
+    var HelpVisibility = false;
+    popupDiv = document.getElementById("popup");
+    // search
+    searchinput = document.getElementById("searchinput");
+    searchinput.addEventListener("keypress", function search(a){
         var key = a.keyCode;
         if(key == 13){
             var query = this.value;
-            if(!cfg_bool[1]){
-                switch(query.substr(0,3)){
-                    case "-g ":
-                        query = query.substr(3);
-                        window.location="https://www.google.com/#q=" + query.replaceChars(" ", "+");
-                        break;
-                    case "-a ":
-                        query = query.substr(3);
-                        window.location="https://duckduckgo.com/?q=" + query.replaceChars(" ", "+");
-                        break;
-                    case "-d ":
-                        query = query.substr(3);
-                        window.location="http://danbooru.donmai.us/posts?tags=" + query.replaceChars(" ", "+");
-                        break;
-                    case "-y ":
-                        query = query.substr(3);
-                        window.location="https://www.youtube.com/results?search_query=" + query.replaceChars(" ", "+");
-                        break;
-                    case "-n ":
-                        query = query.substr(3);
-                        window.location="http://www.nicovideo.jp/search/" + query.replaceChars(" ", "%20");
-                        break;
-                    case "-p ":
-                        query = query.substr(3);
-                        window.location="http://www.pixiv.net/search.php?s_mode=s_tag&word=" + query.replaceChars(" ", "%20");
-                        break;
-                }
-            }else{
-                window.location="https://www.google.com/#q=" + query.replaceChars(" ", "+");
+            switch(query.substr(0,2)){
+                case "-h":
+                    popup(popupDiv,
+                            "-h Shows this list<br>-g Google (default)<br>-a DuckDuckGo<br>-d Danbooru<br>-y YouTube<br>-n niconico<br>-p pixiv",
+                            HelpVisibility);
+                    HelpVisibility = !HelpVisibility;
+                    break;
+                case "-g":
+                    query = query.substr(3);
+                    window.location = "https://www.google.com/#q=" +
+                        query.replaceChars(" ", "+");
+                    break;
+                case "-a":
+                    query = query.substr(3);
+                    window.location = "https://duckduckgo.com/?q=" +
+                        query.replaceChars(" ", "+");
+                    break;
+                case "-d":
+                    query = query.substr(3);
+                    window.location = "http://danbooru.donmai.us/posts?tags=" +
+                        query.replaceChars(" ", "+");
+                    break;
+                case "-y":
+                    query = query.substr(3);
+                    window.location =
+                        "https://www.youtube.com/results?search_query=" +
+                        query.replaceChars(" ", "+");
+                    break;
+                case "-n":
+                    query = query.substr(3);
+                    window.location = "http://www.nicovideo.jp/search/" +
+                        query.replaceChars(" ", "%20");
+                    break;
+                case "-p":
+                    query = query.substr(3);
+                    window.location =
+                        "http://www.pixiv.net/search.php?s_mode=s_tag&word=" +
+                        query.replaceChars(" ", "%20");
+                    break;
+                default:
+                    window.location="https://www.google.com/#q=" +
+                        query.replaceChars(" ", "+");
             }
         }
     });
     
-    document.addEventListener("keypress", function search(a){
+    // jump to search when tab is pressed
+    document.addEventListener("keypress", function(a){
         var key = a.keyCode;
         if(key == 9){
             var search_sqr = document.getElementById("search_sqr")
@@ -115,8 +149,15 @@ window.onload = function(){
         }
     });
 
+    // close popup when clicked
+    popupDiv.addEventListener("click", function(){
+        popup(this, "", HelpVisibility);
+        HelpVisibility = !HelpVisibility;
+    });
+
+    // adding event listeners to squares or expanding them onload
     var sqr = document.querySelectorAll(".sqr");
-    if(!cfg_bool[2]){
+    if(!cfg_bool[1]){
         for(var i = 0; i < sqr.length; ++i) {
             sqr[i].addEventListener("mouseover", expand, false);
             sqr[i].addEventListener("mouseout", contract, false);
@@ -138,6 +179,7 @@ window.onload = function(){
     }
 }
 
+// expanding and contracting squares
 function expand(){
 	var acount = this.getElementsByTagName("a").length;
     var icount = this.getElementsByTagName("input").length;
@@ -157,6 +199,7 @@ function contract(){
 	this.style.borderBottom="0 solid" + cfg[8];
 }
 
+// string replacement
 String.prototype.replaceChars = function(character, replacement){
     var str = this;
     var a;
@@ -171,6 +214,7 @@ String.prototype.replaceChars = function(character, replacement){
     return str;
 }
 
+// cleanup
 window.onunload = function(){
     delete window.cfg;
     delete window.cfg_bool;
